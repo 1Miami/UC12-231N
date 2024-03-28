9d91ebaa787d173a734992aaa86b143b31a12659
Leonardo Lopes (miami) <147109900+1Miami@users.noreply.github.com>
2024-03-27 21:49:02 -0300

    Add files via upload

---
+++ root/js/search.js
@@ -0,0 +1,39 @@
+function validaBusca() {
+    if (document.querySelector('#q').value == '') {
+        alert('Não podia ter deixado em branco a busca!');
+        return false;
+    }
+}
+
+document.querySelector('#form-busca').onsubmit = validaBusca;
+
+var banners = ["./img/roxo.jfif", "./img/mimis.png"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+var timer = setInterval(trocaBanner, 2000);
+var controle = document.querySelector('.pause');
+
+controle.onclick = function () {
+    if (controle.classList.contains('pause')) {
+        clearInterval(timer);
+        controle.classList.remove('pause');
+        controle.classList.add('play');
+        controle.style.color = 'red'; 
+    } else {
+        timer = setInterval(trocaBanner, 2000);
+        controle.classList.remove('play');
+        controle.classList.add('pause');
+        controle.style.color = 'blue'; 
+    }
+    return false;
+};
+
+
+function calculate() {
+
+}
